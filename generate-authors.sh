2e466b04f1c2b8eb75f017ed054b4421f2b66147
Cyborg1999 <andrewk.tonui@gmail.com>
2022-05-17 21:45:32 +0300

    authors

--- generate-authors.sh
+++ generate-authors.sh
@@ -4,11 +4,11 @@ set -e
 # shellcheck disable=SC2128
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
-# see also ".mailmap" for how email and names are deduplicated 
+# see also ".mailmap" for how email and names are deduplicated
 {
 	cat <<-'EOH'
-	# This file lists all individuals having contributed content to the repository.
-	# For how it is generated see `generated-authors.sh`
+		# This file lists all individuals having contributed content to the repository.
+		# For how it is generated see `generated-authors.sh`
 	EOH
 	echo
 	git log --format='%aN <%aE>' | LC_ALLC.UTF-8 sort -uf
